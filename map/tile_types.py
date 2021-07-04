90107040e0bbe012729f49766425108ad51add9e
autopsia <autopsia9@gmail.com>
2021-07-04 03:17:53 -0500

    Add field of view (tutorial part 4).

--- map/tile_types.py
+++ map/tile_types.py
@@ -13,30 +13,37 @@ graphic_dt = np.dtype(
 
 tile_dt = np.dtype(
     [
-        ("walkable", np.bool),
-        ("transparent", np.bool),
-        ("dark", graphic_dt),
+        ("walkable", np.bool),  # if player can move through tile
+        ("transparent", np.bool),  # if tile doesn't block visibility
+        ("dark", graphic_dt),  # Tile not visible
+        ("light", graphic_dt),  # Tile visible
     ]
 )
 
 
 def new_tile(
-        *,
+        *,  # makes use of field name mandatory
         walkable: int,
         transparent: int,
         dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+        light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+
+# holds non visible tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
     walkable=True,
     transparent=True,
     dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
 )
 
 wall = new_tile(
     walkable=False,
     transparent=False,
     dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
 )
